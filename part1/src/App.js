cbcc9d613547e8b63f6776e374a7f28199535824
Janessa Tran <tranjanessa@gmail.com>
2022-07-22 14:56:58 -0700

    Add solution for exercise 1.4

--- part1/src/App.js
+++ part1/src/App.js
@@ -21,31 +21,34 @@ const Content = (props) => {
 };
 
 const Total = (props) => {
-  return <p>Number of exercises {props.count}</p>;
+  const totalExercises = props.parts.reduce((acc, obj) => {
+    return acc + obj.exercises;
+  }, 0);
+  return <p>Number of exercises {totalExercises}</p>;
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
-
-  const exercises = [part1, part2, part3];
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content exercises={exercises} />
-      <Total count={part1.exercises + part2.exercises + part3.exercises} />
+      <Content exercises={parts} />
+      <Total count={parts} />
     </div>
   );
 };
