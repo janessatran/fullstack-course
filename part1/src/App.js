52e5dab715702987e1aceb1c87bb4d1aa837978b
Janessa Tran <tranjanessa@gmail.com>
2022-07-22 14:59:37 -0700

    Add solution for exercise 1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -10,45 +10,48 @@ const Part = ({ title, exerciseCount }) => {
   );
 };
 
-const Content = (props) => {
+const Content = ({ parts }) => {
   return (
     <>
-      {props.exercises.map((part) => {
+      {parts.map((part) => {
         return <Part title={part.name} exerciseCount={part.exercises} />;
       })}
     </>
   );
 };
 
-const Total = (props) => {
-  const totalExercises = props.parts.reduce((acc, obj) => {
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((acc, obj) => {
     return acc + obj.exercises;
   }, 0);
+
   return <p>Number of exercises {totalExercises}</p>;
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content exercises={parts} />
-      <Total count={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
